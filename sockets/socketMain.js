87e1bd1e3fda56ef58fc71e520583ac946f6007b
Anurag Bajaj <anuragb26@gmail.com>
2020-01-30 08:25:03 +0530

    Chore:Set Up Backend Classes

--- sockets/socketMain.js
+++ sockets/socketMain.js
@@ -1,20 +1,34 @@
 // where all our main socket stuff will go
 const { io } = require("../servers");
 const Orb = require("./classes/Orb");
+const Player = require("./classes/Player");
+const PlayerData = require("./classes/PlayerData");
+const PlayerConfig = require("./classes/PlayerConfig");
 
 let orbs = [];
-
+let settings = {
+  defaultOrbs: 500,
+  defaultSpeed: 6,
+  defaultSize: 6,
+  defaultZoom: 1.5,
+  worldWidth: 500,
+  worldHeight: 500
+};
 initGame();
 
 io.on("connect", socket => {
+  let playerConfig = new PlayerConfig(settings);
+  let playerData = new PlayerData(null, settings);
+  let player = new Player(socket.id, playerConfig, playerData);
+
   socket.emit("init", {
     orbs
   });
 });
 
 function initGame() {
-  for (let i = 0; i < 500; i++) {
-    orbs.push(new Orb());
+  for (let i = 0; i < settings.defaultOrbs; i++) {
+    orbs.push(new Orb(settings));
   }
 }
 
